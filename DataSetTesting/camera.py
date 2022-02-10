5c362ca249c2672b3ea662482bf55c024c07694e
Kyle-Smith2 <kdsmith@stu.naperville203.org>
2022-02-10 11:47:23 -0600

    made photos faster and started tf

--- DataSetTesting/camera.py
+++ DataSetTesting/camera.py
@@ -6,17 +6,16 @@ from time import sleep
 #camera = PiCamera()
 #------------------------------------------------------------------
 
-camera.resolution = (200, 200)
+camera.resolution = (300,300)
 camera.framerate = 60
 camera.brightness = 60
 camera.saturation = 60 
 camera.rotation = 180
 
-
 i = 0
-for i in range(5):
+for i in range(50):
     sleep(0)
-    camera.capture('/home/pi/MachineLearningDoorbell/DataSetTesting/dataset1'+str(i)+'.jpg', format = None, use_video_port=False)
+    camera.capture('/home/pi/MachineLearningDoorbell/DataSetTesting/dataset1/image'+str(i)+'.jpg', format = None, use_video_port=True)
     print(i)
     
 print("done")
---
+++ DataSetTesting/tftest.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+hello = tf.constant("elo tensaflo")
+#sess = tf.Session()
+print (sess.run(hello))
