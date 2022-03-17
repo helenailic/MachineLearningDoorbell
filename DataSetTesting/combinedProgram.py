21641781923d989c6a91577ea3eeca9962b46ad2
JasonMcKinlay <jamckinlay@stu.naperville203.org>
2022-03-17 11:48:08 -0500

    almost done with combined program

--- DataSetTesting/combinedProgram.py
+++ DataSetTesting/combinedProgram.py
@@ -5,7 +5,7 @@ from time import sleep
 import numpy
 from PIL import Image
 
-
+# -----uncomment the following when using a pi connected to the camera-----
 #camera = PiCamera()
 
 #camera.resolution = (300,300)
@@ -18,17 +18,48 @@ from PIL import Image
 #    for i in range(10):
 #        sleep(0)
 #        camera.capture('/home/pi/MachineLearningDoorbell/DataSetTesting/dataset1/image'+str(i)+'.jpg', format = None, use_video_port=True)
-#recognizeFace()    
     
-#def recognizeFace():
-y = 0
-n = 0
-for i in range(10):
-    #img = open('/home/pi/MachineLearningDoorbell/DataSetTesting/dataset1/image'+str(i)+'.jpg')
-    img = Image.open('/home/pi/MachineLearningDoorbell/DataSetTesting/dataset1/image'+str(i)+'.jpg', "r")
-    #width, height = img.size
-    pixel_values = list(img.getdata())
-    for j in range(len(pixel_values)):
-        #print(j)
+def recognizeFace():
+    y = 0
+    n = 0
+    for i in range(10):
+        #img = open('/home/pi/MachineLearningDoorbell/DataSetTesting/dataset1/image'+str(i)+'.jpg')
+        img = Image.open('/home/pi/MachineLearningDoorbell/DataSetTesting/dataset1/image'+str(i)+'.jpg', "r")
+        width, height = img.size
+        pixel_values = list(img.getdata())
+        #print(pixel_values)
+        avgR = 0
+        avgG = 0
+        avgB = 0
+        
+        for j in range(len(pixel_values)):
+            avgR += pixel_values[j][0]
+            avgG += pixel_values[j][1]
+            avgB += pixel_values[j][2]
+            #print(avg)
+        avgR /= (width*height)
+        avgG /= (width*height)
+        avgB /= (width*height)
+        
+        avg = (avgR, avgG, avgB)
+        if avg > (127, 127, 127):
+            y += 1
+        if avg <= (127, 127, 127):
+            n += 1
+            
+        if y > n:
+            print("yes light")
+            sound = vlc.MediaPlayer("boing.wav")
+            sound.stop()
+            sound.play()
+        if n > y:
+            print("no light")
+        if n == y:
+            print("helena wtf do i do in this situation")
+            
+        
     #print(pixel_values)
+
+recognizeFace()
+
         
