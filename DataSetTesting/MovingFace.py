78d785aff0f40129dd633ee29b8d2f1490494baa
Kyle-Smith2 <70662235+Kyle-Smith2@users.noreply.github.com>
2022-04-19 11:43:00 -0500

    updated MovingFace
    bro

--- DataSetTesting/MovingFace.py
+++ DataSetTesting/MovingFace.py
@@ -4,6 +4,7 @@ from picamera import PiCamera
 from time import sleep
 from PIL import Image
 import math
+sound = vlc.MediaPlayer("/home/pi/boing.mp3")
 
 #start camera
 camera = PiCamera()
@@ -16,26 +17,26 @@ camera.rotation = 180
 # isSamePixel method that takes in two pixels and checks if they are close
 def isSamePixel(pixel1, pixel2):
   isSame = True
-  if abs(pixel1[0] - pixel2[0]) > 5:
+  if abs(pixel1[0] - pixel2[0]) > 15:
     isSame = False
-  if abs(pixel1[1] - pixel2[1]) > 5:
+  if abs(pixel1[1] - pixel2[1]) > 15:
     isSame = False
-  if abs(pixel1[2] - pixel2[2]) > 5:
+  if abs(pixel1[2] - pixel2[2]) > 15:
     isSame = False
   return isSame
 
 # take an example image
-camera.capture('/home/pi/MachineLearningDoorbell/DataSetTesting/dataset3/orig.jpg', format = None, use_video_port=True)
+camera.capture('/home/pi/orig.jpg', format = None, use_video_port=True)
 sleep(1)
 
 # loop the following 5 times:
 n = 0
 i = 0
-for i in range(5):
+for i in range(500):
   # take another image and see if more than 100 pixels changed from the first image
-  camera.capture('/home/pi/MachineLearningDoorbell/DataSetTesting/dataset3/new.jpg', format = None, use_video_port=True)
-  original = Image.open('/home/pi/MachineLearningDoorbell/DataSetTesting/dataset3/orig.jpg', "r")
-  new = Image.open('/home/pi/MachineLearningDoorbell/DataSetTesting/dataset3/new.jpg', "r")
+  camera.capture('/home/pi/new.jpg', format = None, use_video_port=True)
+  original = Image.open('/home/pi/orig.jpg', "r")
+  new = Image.open('/home/pi/new.jpg', "r")
   width1, height1 = original.size
   width2, height2 = new.size
   pixel_values1 = list(original.getdata())
@@ -49,20 +50,16 @@ for i in range(5):
     else:
         changedPixels += 1
 
-  if changedPixels > 1000:
+  if changedPixels > 10000:
     n += 1;
-    print("yay"+str(changedPixels))
-  else:
-    print ("nooo")
-  
+    print(changedPixels)
+    sound.stop()
+    sound.play()
+
   # take a new reference image
-  camera.capture('/home/pi/MachineLearningDoorbell/DataSetTesting/dataset3/orig.jpg', format = None, use_video_port=True)
+  camera.capture('/home/pi/orig.jpg', format = None, use_video_port=True)
   
   # wait 1 second
   sleep(1)
 
 # if n >= 3, then play the sound effect because there was motion
-if n >= 3:
-  sound = vlc.MediaPlayer("boing.wav")
-  sound.stop()
-  sound.play()
